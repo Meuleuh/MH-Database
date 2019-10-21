1bf1a40b41582613082fa711e2f900c9d2bbf9e3
Meuleuh <32850732+Meuleuh@users.noreply.github.com>
2019-10-21 13:46:33 -0400

    Added also every french name for the MH3U GS to the file for further object creation

--- MH Database/MH Database/Ressources Files/Lang/Game Elements/Weapons/Great Sword/MH3U.cs
+++ MH Database/MH Database/Ressources Files/Lang/Game Elements/Weapons/Great Sword/MH3U.cs
@@ -5,279 +5,460 @@ namespace MH_Database.Ressources.Lang.Weapons.GS
     class MH3U
     {
         //Iron Sword
+        //Épée de fer
         internal static MultiLangWeapon ironSword = new MultiLangWeapon
             (
-            frAllTier:"Épée de fer",
-            enAllTier:"Iron Sword"
+            enAllTier:"Iron Sword",
+            frAllTier: "Épée de fer"
             );
 
         //Iron Sword+
+        //Épée de fer +
         internal static MultiLangWeapon ironSwordPlus = new MultiLangWeapon
             (
-            frAllTier:"Épée de fer +",
-            enAllTier:"Iron Sword+"
+            enAllTier:"Iron Sword+",
+            frAllTier: "Épée de fer +"
             );
 
         //Buster Sword
+        //Épée découpeuse
+        internal static MultiLangWeapon busterSword = new MultiLangWeapon
+            (
+            enAllTier: "Buster Sword",
+            frAllTier: "Épée découpeuse"
+            );
 
         //Buster Sword+
+        //Épée découpeuse +
+        internal static MultiLangWeapon busterSwordPlus = new MultiLangWeapon
+            (
+            enAllTier: "Buster Sword+",
+            frAllTier: "Épée découpeuse +"
+            );
 
         //Buster Blade
+        //Lame découpeuse
+        internal static MultiLangWeapon busterBlade = new MultiLangWeapon
+            (
+            enAllTier: "Buster Blade",
+            frAllTier: "Lame découpeuse"
+            );
 
         //Ravager Blade
+        //Lame ravageuse
+        internal static MultiLangWeapon ravagerBlade = new MultiLangWeapon
+            (
+            enAllTier: "Ravager Blade",
+            frAllTier: "Lame ravageuse"
+            );
 
         //Lacerator Blade
+        //Lame Lacerator
+        internal static MultiLangWeapon laceratorBlade = new MultiLangWeapon
+            (
+            enAllTier: "Lacerator Blade",
+            frAllTier: "Lame Lacerator"
+            );
 
         //Devastator Blade
+        //Lame dévastatrice
+        internal static MultiLangWeapon devastatorBlade = new MultiLangWeapon
+            (
+            enAllTier: "Devastator Blade",
+            frAllTier: "Lame dévastatrice"
+            );
 
         //Chrome Razor
+        //Rasoir chrome
+        internal static MultiLangWeapon chromeRazor = new MultiLangWeapon
+            (
+            enAllTier: "Chrome Razor",
+            frAllTier: "Rasoir chrome"
+            );
 
         //Chrome Quietus
+        //Quietus chrome
+        internal static MultiLangWeapon chromeQuietus = new MultiLangWeapon
+            (
+            enAllTier: "Chrome Quietus",
+            frAllTier: "Quietus chrome"
+            );
 
         //Brazenwall
+        //Lame d'airain
+        internal static MultiLangWeapon brazenwall = new MultiLangWeapon
+            (
+            enAllTier: "Brazenwall",
+            frAllTier: "Lame d'airain"
+            );
 
         //Crimsonwall
+        //Mur pourpre
 
         //Cragscliff
+        //Rochetaille
 
         //Goliath's Scream
+        //Cri de Goliath
 
         //Hidden Blade
+        //Lame cachée
 
         //Dark of Night
+        //Sombre nuit
 
         //Avidya Great Sword
+        //Grande épée Avidya
 
         //Type 41 Wyvernator
+        //Wyvernator type 41
 
+        //Remalgalypse
         //Remalgalypse
 
         //Remalgagorgon
+        //Remalgagorgon
 
         //Rugged Great Sword
+        //Grande épée sauvage
 
         //Chieftain's Grt Swd
+        //Grande épée de chef
 
         //High Chief's Grt Swd
+        //Grande épée grd chef
 
         //Lagiacrus Blade
+        //Lame Lagiacrus
 
         //Lagiacrus Blade+
+        //Lame Lagiacrus +
 
         //High Lagia Blade
+        //Grande lame Lagia
 
         //Lagia Lightning
+        //Éclair Lagia
 
         //Lagia Wildfire
+        //Feu sauvage Lagia
 
         //Neo Lagia Blade
+        //Lame néo-Lagia
 
         //Lagia Fulmination
+        //Fulmination Lagia
 
         //Usurper's Storm
+        //Tempête d'usurpateur
 
         //Despot's Blackstorm
+        //Tempête du despote
 
         //Oppressor's Forger
+        //Forgeron oppresseur
 
+        //Orcus Tonitrus
         //Orcus Tonitrus
 
         //Nether Great Sword
+        //Grande épée Nether
 
         //Nether Lufactrus
+        //Lufactrus Nether
 
         //Brimstren Drakepride
+        //Orgueil du dragon
 
         //Stygian Acedia
+        //Acedia stygienne
 
         //Carapace Sword
+        //Épée carapace
 
         //Carapace Blade
+        //Lame carapace
 
         //Barroth Smasher
+        //Écraseur Barroth
 
         //Aurora Blade
+        //Lame aurore
 
         //Nothern Lights
+        //Aurore boréale
 
         //Nothern Cross
+        //Croix du nord
 
         //Viking Hornsword
+        //Épée en corne Viking
 
         //Ancharius
+        //Épée Ancharius
 
         //Excalius Sword
+        //Épée Excalius
 
         //Bone Blade
+        //Lame en os
 
         //Bone Blade+
+        //Lame en os +
 
         //Jawblade
+        //Lame mâchoire
 
         //Giant Jawblade
+        //Lame mâchoire géante
 
         //Red Wing
+        //Aile rouge
 
         //Rathalos Firesword
+        //Épée de feu Rathalos
 
         //Rathalos Flamesword
+        //Épée flamme Rathalos
 
         //Blue Wing
+        //Aile bleue
 
         //Rathalos Glinsword
+        //Flam bleue Rathalos
 
         //Rathalos Gleamsword
+        //Flam argent Rathalos
 
         //Dios Blade
+        //Lame Dios
 
         //Dios Blade+
+        //Lame Dios +
 
         //Demolition Sword
+        //Épée de démolition
 
         //Myxo Demolisher
+        //Démolisseur Myxo
 
         //Valkyrie Blade
+        //Lame Valkyrie
 
+        //Sieglinde
         //Sieglinde
 
         //High Sieglinde
+        //Grande Sieglinde
 
+        //Titania
         //Titania
 
         //Blushing Dame
+        //Dame rougissante
 
+        //Ljósálfar
         //Ljósálfar
 
         //Brünnhilde
+        //Brünnhilde
 
+        //Siegmund
         //Siegmund
 
         //High Siegmund
+        //Grand Siegmund
 
+        //Reddnaught
         //Reddnaught
 
         //Auberon
+        //Auberon
 
         //Pale Kaiser
+        //Kaiser pâle
 
         //Golem Blade
+        //Lame Golem
 
         //Blade of Talos
+        //Lame de Talos
 
         //Blade of Tartarus
+        //Lame de Tartarus
 
         //Wyvern Jawblade
+        //Lame mâchoire Wyvern
 
         //Quarrel Hornsword
+        //Épée en corne colère
 
         //Quarrel Hornsword+
+        //Épée corne colère +
 
+        //Double Diablos
         //Double Diablos
 
         //Cera Cigil
+        //Cera Cigil
 
         //Cera Cymmmetry
+        //Cera Symmetry
 
         //Ludroth Bone Sword
+        //Épée en os Ludroth
 
         //Cataclysm Sword
+        //Épée cataclysme
 
         //Cataclysm Blade
+        //Lame cataclysme
 
         //Icicle Fang
+        //Croc stalactite
 
         //Icicle Fang+
+        //Croc stalactite +
 
         //Paladire
+        //Sinistre
 
         //Galespike
+        //Pointebrise
 
         //Simoom Sandbarb
+        //Simoun sable
+
+        //Simoom Sandbitter
+        //Simoun Mord sable
 
         //Finblade
+        //Lame nageoire
 
         //Plesioth Watercutter
+        //Fend l'eau Plesioth
 
         //Plesioth Aquablade
+        //Lame eau Plesioth
 
         //Plesioth Lullaby
+        //Berceuse Plesioth
 
         //Plesioth Lullabane
+        //Berce fléau Plesioth
 
         //Frozen Speartuna
+        //Lancethon gelée
 
         //Freezer Speartuna
+        //Lancethon de gel
 
         //Xiphias Gladius
+        //Gladius Xiphias
 
+        //Vulcanis
         //Vulcanis
 
         //Vulcanis+
+        //Vulcanis +
 
         //Vulcanvil
+        //Vulcanclume
 
+        //Vulcamagnon
         //Vulcamagnon
 
         //Vulcatastrophe
+        //Vulcatastrophe
 
         //Vulca Vendetta
+        //Vulca vendetta
 
         //Rusted Great Sword
+        //Grande épée rouillée
 
         //Tarnished Great Sword
+        //Grande épée ternie
 
         //Ancient Blade
+        //Lame antique
 
         //Elder Monument
+        //Monument antique
 
         //Eternal Glyph
+        //Glyphe éternel
 
         //Wyvern's Perch
+        //Perche Wyvern
 
         //Roguish Deathcap
+        //Calot de mort malin
 
         //Lordly Deathcap
+        //Calot de mort noble
 
+        //Houma no Tsurugi
         //Houma no Tsurugi
 
         //Tenma no Tsurugi
+        //Tenma no Tsurugi
 
         //Berserker Sword
+        //Lame déchaînée
 
         //Anguish
+        //Supplice
 
         //Nero's Anguish
+        //Supplice de Néron
 
         //Lion's Bane
+        //Fléau du lion
 
         //Alatreon Great Sword
+        //Grande épée Alatreon
 
         //Alatreon Revolution
+        //Révolution Alatreon
 
         //Altheos Evolutia
+        //Evolutia Alatreon
 
         //Amethyst Blade
+        //Lame améthyste
 
         //Miasmethyst
+        //Miasméthyste
 
         //Anima Reaper
+        //Faucheur d'âme
 
-        //Elsenfaust
+        //Eisenfaust
+        //Eisenfaust
 
         //Worn Great Sword
+        //Grande épée usée
 
         //Weathered Great Swd
+        //Grande épée effritée
 
         //Epitaph Blade
+        //Lame épitaphe
 
         //Epitaph Eternal
+        //Épitaphe éternelle
 
         //Eclipse Blade
+        //Lame de l'éclipse
 
         //Merak's Asterism
+        //Astérisme de Merak
 
         //Megiddo Blaze
+        //Brasier Megiddo
 
         //The Depotheosis
+        //Depothosis
     }
 }
