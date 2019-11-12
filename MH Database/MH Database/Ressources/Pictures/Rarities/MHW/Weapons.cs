4b0368292565caa2041f2523678d667844c0d245
Meuleuh <32850732+Meuleuh@users.noreply.github.com>
2019-11-12 12:20:12 -0500

    Started indexing

---
+++ MH Database/MH Database/Ressources/Pictures/Rarities/MHW/Weapons.cs
@@ -0,0 +1,75 @@
+﻿using System.Drawing;
+
+namespace MH_Database.Ressources.Pictures.Rarities.MHW
+{
+    internal class WeaponsIcons
+    {
+        readonly internal Bitmap[] Bow = new Bitmap[13]
+        {
+            null,                   //We keep 'null' so that [1] return the rarity 1 icon
+            Weapons.rare_1_bow_mhw_tree,
+            Weapons.rare_2_bow_mhw_tree,
+            Weapons.rare_3_bow_mhw_tree,
+            Weapons.rare_4_bow_mhw_tree,
+            Weapons.rare_5_bow_mhw_tree,
+            Weapons.rare_6_bow_mhw_tree,
+            Weapons.rare_7_bow_mhw_tree,
+            Weapons.rare_8_bow_mhw_tree,
+            Weapons.rare_9_mhw_bow_wiki,
+            Weapons.rare_10_mhw_bow_wiki,
+            Weapons.rare_11_mhw_bow_wiki,
+            Weapons.rare_12_mhw_bow_wiki
+        };
+
+        readonly internal Bitmap[] Charge_Blade = new Bitmap[13]
+        {
+            null,                   //We keep 'null' so that [1] return the rarity 1 icon
+            Weapons.rare_1_charge_blade_mhw_tree,
+            Weapons.rare_2_charge_blade_mhw_tree,
+            Weapons.rare_3_charge_blade_mhw_tree,
+            Weapons.rare_4_charge_blade_mhw_tree,
+            Weapons.rare_5_charge_blade_mhw_tree,
+            Weapons.rare_6_charge_blade_mhw_tree,
+            Weapons.rare_7_charge_blade_mhw_tree,
+            Weapons.rare_8_charge_blade_mhw_tree,
+            Weapons.rare_9_mhw_charge_blade_wiki,
+            Weapons.rare_10_mhw_charge_blade_wiki,
+            Weapons.rare_11_mhw_charge_blade_wiki,
+            Weapons.rare_12_mhw_charge_blade_wiki
+        };
+
+        readonly internal Bitmap[] Dual_Blades = new Bitmap[13]
+        {
+            null,                   //We keep 'null' so that [1] return the rarity 1 icon
+            Weapons.rare_1_dual_blades_mhw_tree,
+            Weapons.rare_2_dual_blades_mhw_tree,
+            Weapons.rare_3_dual_blades_mhw_tree,
+            Weapons.rare_4_dual_blades_mhw_tree,
+            Weapons.rare_5_dual_blades_mhw_tree,
+            Weapons.rare_6_dual_blades_mhw_tree,
+            Weapons.rare_7_dual_blades_mhw_tree,
+            Weapons.rare_8_dual_blades_mhw_tree,
+            Weapons.rare_9_mhw_dualblades_wiki,
+            Weapons.rare_10_mhw_dualblades_wiki,
+            Weapons.rare_11_mhw_dualblades_wiki,
+            Weapons.rare_12_mhw_dualblades_wiki
+        };
+
+        readonly internal Bitmap[] Great_Sword = new Bitmap[13]
+{
+            null,                   //We keep 'null' so that [1] return the rarity 1 icon
+            Weapons.rare_1_greatsword_mhw_tree,
+            Weapons.rare_2_greatsword_mhw_tree,
+            Weapons.rare_3_greatsword_mhw_tree,
+            Weapons.rare_4_greatsword_mhw_tree,
+            Weapons.rare_5_greatsword_mhw_tree,
+            Weapons.rare_6_greatsword_mhw_tree,
+            Weapons.rare_7_greatsword_mhw_tree,
+            Weapons.rare_8_greatsword_mhw_tree,
+            Weapons.rare_9_mhw_greatsword_wiki,
+            Weapons.rare_10_mhw_greatsword_wiki,
+            Weapons.rare_11_mhw_greatsword_wiki,
+            Weapons.rare_12_mhw_greatsword_wiki
+};
+    }
+}
