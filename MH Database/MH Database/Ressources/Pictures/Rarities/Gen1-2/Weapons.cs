f1537dd6810971a9393802313a78b3081690e1e5
Meuleuh <32850732+Meuleuh@users.noreply.github.com>
2019-11-18 09:36:01 -0500

    Icons for gen 1 and 2 indexing completed

---
+++ MH Database/MH Database/Ressources/Pictures/Rarities/Gen1-2/Weapons.cs
@@ -0,0 +1,138 @@
+﻿using System.Drawing;
+
+namespace MH_Database.Ressources.Pictures.Rarities.Gen1_2
+{
+    internal class WeaponsIcons
+    {
+        readonly internal Bitmap[] Bow = new Bitmap[8]
+        {
+            null,                   //We keep 'null' so that [1] return the rarity 1 icon
+            Weapons.Bow_Rarity_1,
+            Weapons.Bow_Rarity_1,
+            Weapons.Bow_Rarity_1,
+            Weapons.Bow_Rarity_4,
+            Weapons.Bow_Rarity_5,
+            Weapons.Bow_Rarity_6,
+            Weapons.Bow_Rarity_7
+        };
+        readonly internal Bitmap[] Dual_Blades = new Bitmap[8]
+        {
+            null,                   //We keep 'null' so that [1] return the rarity 1 icon
+            Weapons.Dual_Blades_Rarity_1,
+            Weapons.Dual_Blades_Rarity_1,
+            Weapons.Dual_Blades_Rarity_1,
+            Weapons.Dual_Blades_Rarity_4,
+            Weapons.Dual_Blades_Rarity_5,
+            Weapons.Dual_Blades_Rarity_6,
+            Weapons.Dual_Blades_Rarity_7
+        };
+
+        readonly internal Bitmap[] Great_Sword = new Bitmap[8]
+        {
+            null,                   //We keep 'null' so that [1] return the rarity 1 icon
+            Weapons.Great_Sword_Rarity_1,
+            Weapons.Great_Sword_Rarity_1,
+            Weapons.Great_Sword_Rarity_1,
+            Weapons.Great_Sword_Rarity_4,
+            Weapons.Great_Sword_Rarity_5,
+            Weapons.Great_Sword_Rarity_6,
+            Weapons.Great_Sword_Rarity_7
+        };
+
+        readonly internal Bitmap[] Gunlance = new Bitmap[8]
+        {
+            null,                   //We keep 'null' so that [1] return the rarity 1 icon
+            Weapons.Gunlance_Rarity_1,
+            Weapons.Gunlance_Rarity_1,
+            Weapons.Gunlance_Rarity_1,
+            Weapons.Gunlance_Rarity_4,
+            Weapons.Gunlance_Rarity_5,
+            Weapons.Gunlance_Rarity_6,
+            Weapons.Gunlance_Rarity_7
+        };
+
+        readonly internal Bitmap[] Hammer = new Bitmap[8]
+        {
+            null,                   //We keep 'null' so that [1] return the rarity 1 icon
+            Weapons.Hammer_Rarity_1,
+            Weapons.Hammer_Rarity_1,
+            Weapons.Hammer_Rarity_1,
+            Weapons.Hammer_Rarity_4,
+            Weapons.Hammer_Rarity_5,
+            Weapons.Hammer_Rarity_6,
+            Weapons.Hammer_Rarity_7
+        };
+
+        readonly internal Bitmap[] Heavy_Bowgun = new Bitmap[8]
+        {
+            null,                   //We keep 'null' so that [1] return the rarity 1 icon
+            Weapons.Heavy_Bowgun_Rarity_1,
+            Weapons.Heavy_Bowgun_Rarity_1,
+            Weapons.Heavy_Bowgun_Rarity_1,
+            Weapons.Heavy_Bowgun_Rarity_4,
+            Weapons.Heavy_Bowgun_Rarity_5,
+            Weapons.Heavy_Bowgun_Rarity_6,
+            Weapons.Heavy_Bowgun_Rarity_7
+        };
+
+        readonly internal Bitmap[] Hunting_Horn = new Bitmap[8]
+        {
+            null,                   //We keep 'null' so that [1] return the rarity 1 icon
+            Weapons.Hunting_Horn_Rarity_1,
+            Weapons.Hunting_Horn_Rarity_1,
+            Weapons.Hunting_Horn_Rarity_1,
+            Weapons.Hunting_Horn_Rarity_4,
+            Weapons.Hunting_Horn_Rarity_5,
+            Weapons.Hunting_Horn_Rarity_6,
+            Weapons.Hunting_Horn_Rarity_7
+        };
+
+        readonly internal Bitmap[] Lance = new Bitmap[8]
+        {
+            null,                   //We keep 'null' so that [1] return the rarity 1 icon
+            Weapons.Lance_Rarity_1,
+            Weapons.Lance_Rarity_1,
+            Weapons.Lance_Rarity_1,
+            Weapons.Lance_Rarity_4,
+            Weapons.Lance_Rarity_5,
+            Weapons.Lance_Rarity_6,
+            Weapons.Lance_Rarity_7
+        };
+
+        readonly internal Bitmap[] Light_Bowgun = new Bitmap[8]
+        {
+            null,                   //We keep 'null' so that [1] return the rarity 1 icon
+            Weapons.Light_Bowgun_Rarity_1,
+            Weapons.Light_Bowgun_Rarity_1,
+            Weapons.Light_Bowgun_Rarity_1,
+            Weapons.Light_Bowgun_Rarity_4,
+            Weapons.Light_Bowgun_Rarity_5,
+            Weapons.Light_Bowgun_Rarity_6,
+            Weapons.Light_Bowgun_Rarity_7
+        };
+
+        readonly internal Bitmap[] Long_Sword = new Bitmap[8]
+        {
+            null,                   //We keep 'null' so that [1] return the rarity 1 icon
+            Weapons.Long_Sword_Rarity_1,
+            Weapons.Long_Sword_Rarity_1,
+            Weapons.Long_Sword_Rarity_1,
+            Weapons.Long_Sword_Rarity_4,
+            Weapons.Long_Sword_Rarity_5,
+            Weapons.Long_Sword_Rarity_6,
+            Weapons.Long_Sword_Rarity_7
+        };
+
+        readonly internal Bitmap[] Sword_and_Shield = new Bitmap[8]
+        {
+            null,                   //We keep 'null' so that [1] return the rarity 1 icon
+            Weapons.Sword_and_Shield_Rarity_1,
+            Weapons.Sword_and_Shield_Rarity_1,
+            Weapons.Sword_and_Shield_Rarity_1,
+            Weapons.Sword_and_Shield_Rarity_4,
+            Weapons.Sword_and_Shield_Rarity_5,
+            Weapons.Sword_and_Shield_Rarity_6,
+            Weapons.Sword_and_Shield_Rarity_7
+        };
+    }
+}
