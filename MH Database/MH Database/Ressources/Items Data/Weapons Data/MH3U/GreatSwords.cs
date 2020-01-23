50a5812f2a4dc13f03d6f2a0bbd821da2a8809b1
Meuleuh <32850732+Meuleuh@users.noreply.github.com>
2020-01-23 08:30:49 -0500

    Added some MH3U GS stats

--- MH Database/MH Database/Ressources/Items Data/Weapons Data/MH3U/GreatSwords.cs
+++ MH Database/MH Database/Ressources/Items Data/Weapons Data/MH3U/GreatSwords.cs
@@ -14,7 +14,7 @@ namespace MH_Database.Ressources.Items_Data
                 internal readonly Weapon ironSword = new Weapon
                     (
                     name: greatSwordsName.ironSword,
-                    rarity: new Classes.Elements.Rarity(1, Classes.Elements.Rarity.tag.Gen_One_Two_Great_Sword),
+                    rarity: new Classes.Elements.Rarity(1, Classes.Elements.Rarity.tag.Gen_Three_Four_Great_Sword),
                     picture: null,
                     attack: 336,
                     true_attack: 70,
@@ -28,6 +28,95 @@ namespace MH_Database.Ressources.Items_Data
                     }
                     );
 
+                //Iron Sword+
+                internal readonly Weapon ironSwordPlus = new Weapon
+                    (
+                    name: greatSwordsName.ironSwordPlus,
+                    rarity: new Classes.Elements.Rarity(1, Classes.Elements.Rarity.tag.Gen_Three_Four_Great_Sword),
+                    picture: null,
+                    attack: 384,
+                    true_attack: 80,
+                    defense: 0,
+                    affinity: 0,
+                    slots: new int[3]
+                    {
+                            0,
+                            0,
+                            0
+                    }
+                    );
+
+                //Buster Sword
+                internal readonly Weapon busterSword = new Weapon
+                    (
+                    name: greatSwordsName.busterSword,
+                    rarity: new Classes.Elements.Rarity(1, Classes.Elements.Rarity.tag.Gen_Three_Four_Great_Sword),
+                    picture: null,
+                    attack: 432,
+                    true_attack: 90,
+                    defense: 0,
+                    affinity: 0,
+                    slots: new int[3]
+                    {
+                        0,
+                        0,
+                        0
+                    }
+                    );
+
+                //Buster Sword+
+                internal readonly Weapon busterSwordPlus = new Weapon
+                    (
+                    name: greatSwordsName.busterSwordPlus,
+                    rarity: new Classes.Elements.Rarity(1, Classes.Elements.Rarity.tag.Gen_Three_Four_Great_Sword),
+                    picture: null,
+                    attack: 528,
+                    true_attack: 110,
+                    defense: 0,
+                    affinity: 0,
+                    slots: new int[3]
+                    {
+                        0,
+                        0,
+                        0
+                    }
+                    );
+
+                //Buster Blade
+                internal readonly Weapon busterBlade = new Weapon
+                    (
+                    name: greatSwordsName.busterBlade,
+                    rarity: new Classes.Elements.Rarity(3, Classes.Elements.Rarity.tag.Gen_Three_Four_Great_Sword),
+                    picture: null,
+                    attack: 624,
+                    true_attack: 130,
+                    defense: 0,
+                    affinity: 0,
+                    slots: new int[3]
+                    {
+                        0,
+                        0,
+                        0
+                    }
+                    );
+
+                //Ravager Blade
+                internal readonly Weapon ravagerBlade = new Weapon
+                    (
+                    name: greatSwordsName.ravagerBlade,
+                    rarity: new Classes.Elements.Rarity(3, Classes.Elements.Rarity.tag.Gen_Three_Four_Great_Sword),
+                    picture: null,
+                    attack: 672,
+                    true_attack: 140,
+                    defense: 12,
+                    affinity: 5,
+                    slots: new int[3]
+                    {
+                        1,
+                        0,
+                        0
+                    }
+                    );
             }
         }
     }
